78eb4649847a257ad8e502ba58c7b1bb8887468c
Prathamesh Palav <prathameshpalav123@gmail.com>
2018-12-11 11:03:10 +0530

    Update config.php
    Feeds changed from 5 to 50

--- config.php
+++ config.php
@@ -6,6 +6,6 @@
 * LIMITS - holds feed limits for each entities in game
 * 
 * */ 
-const GAME_ROUNDS = 5, 
+const GAME_ROUNDS = 50, 
 		LIMITS = array('FARMER' => 15, 'COW' => 10, 'BUNNY' => 8),
 		ENTITIES = array('farmer', 'cow1', 'cow2', 'bunny1', 'bunny2', 'bunny3');
